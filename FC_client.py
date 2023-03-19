ce0e8375e9945e3289acd15a9c1bab67af3fb45f
Ruslan <xeont88@gmail.com>
2023-03-19 14:06:51 +0300

    немного добавили

--- FC_client.py
+++ FC_client.py
@@ -2,26 +2,16 @@
     Клиент для консольной игры "Бойцовский Клуб".
 
     Протокол общения:
+    Регистрация клиента:    'registr|' + name
+    Помощь:                 'help'
+    Запрос информации:      'info'
 '''
 
 from socket import *
 
-server_address = ('localhost', 5400)
 
-print('Игра "Бойцовский Клуб!"')
-
-name = input('Введите имя для персонажа:\n')
-
-# TODO: регистрация игрока на сервере.
-
-while True:
-    msg_hit = input('Введите место удара (1,2,3,4):\n')
-    # TODO: добавить поддержку help
-    msg_block = input('Введите защищаемое место (1,2,3,4):\n')
-
-    # Посылка для сервера вида: "_место_удара_ _защищаемое_место_"
-    bin_msg = bytes(msg_hit + ' ' + msg_block, 'utf-8')
-
+def send_msg(msg: str):
+    bin_msg = bytes(msg, 'utf-8')
     client = socket(AF_INET, SOCK_STREAM)
 
     try:
@@ -39,4 +29,28 @@ while True:
         # Обязательно закрываем соединение с сервером
         client.close()
 
-# ПИРИФ!
+
+server_address = ('localhost', 5400)
+
+print('Игра "Бойцовский Клуб!"')
+
+name = input('Введите имя для персонажа:\n')
+
+send_msg('registr|' + name)
+
+while True:
+    # Проверка на корректность данных
+    step = True
+    while step:
+        msg_hit = input('Введите место удара (1,2,3,4):\n')
+        # TODO: добавить поддержку help
+        msg_block = input('Введите защищаемое место (1,2,3,4):\n')
+        # Если удар и защита выбраны корректно - выходим из проверки
+        if 0 < int(msg_hit) < 5 and 0 < int(msg_block) < 5:
+            step = False
+        else:
+            # Оповещаем пользователя, о некорректности введенных данных
+            print('Места для удара/защиты выбраны неверно!!!!!')
+
+    # Посылка для сервера вида: "_место_удара_ _защищаемое_место_"
+    send_msg(msg_hit + ' ' + msg_block)
--- FC_server.py
+++ FC_server.py
@@ -3,9 +3,20 @@
 
     Протокол общения:
         "_место_удара_ _защищаемое_место_"  - ход игрока
+    Регистрация клиента:    'registr|' + name
+    Помощь:                 'help'
+    Запрос информации:      'info'
 '''
 
 from socket import *
+
+
+# Регистрация клиента на сервере
+clients = {}
+def client_reg(name, ip):
+    global clients
+    clients.update({ip: name})
+
 
 # Настройка сокета
 socket_object = socket(AF_INET, SOCK_STREAM)
@@ -20,22 +31,30 @@ while True:
     connection, address = socket_object.accept()
     # Получаем данные от клиента
     bin_data = connection.recv(1024)
-    str_data = bin_data.decode('utf-8')     # 1-4 1-4
+    str_data = bin_data.decode('utf-8')  # 1-4 1-4
 
     print(str_data)
+
+    # Разбиваем входящее сообщение на параметры
+    commands = str_data.split('|')
 
     # получаем ip клиента
     ip_address = address[0]
 
-    # Список с местами для удара/защиты
-    hit_markers = ['голова', "торс", "пояс", "ноги"]
-    "3 2"
-    data_list = str_data.split()    # формируем список команд от клиента
-    msg_hit = int(data_list[0])     # Выделяем номер места для удара
-    msg_block = int(data_list[1])   # Выделяем номер места для защиты
+    if commands[0] == 'registr':
+        # Регистрация клиента (пользователя)
+        client_reg(commands[1], ip_address)
+        answer = f'Зарегистрирован игрок по имени {clients[ip_address]}'
+    else:
+        # Список с местами для удара/защиты
+        hit_markers = ['голову', "торс", "пояс", "ноги"]
+        "3 2"
+        data_list = str_data.split()  # формируем список команд от клиента
+        msg_hit = int(data_list[0]) - 1  # Выделяем номер места для удара
+        msg_block = int(data_list[1]) - 1  # Выделяем номер места для защиты
 
-    # Формируем сообщение для ответа клиенту
-    answer = f'Игрок 1 ударил в {hit_markers[msg_hit]} защитил {hit_markers[msg_block]}'
+        # Формируем сообщение для ответа клиенту
+        answer = f'{clients[ip_address]} ударил в {hit_markers[msg_hit]} защитил {hit_markers[msg_block]}'
 
     # Отправляем ответ клиенту
     connection.send(answer.encode('utf-8'))
